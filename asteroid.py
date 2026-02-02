90ca1ae5d55df6749048f26624cf34a6685a4b4c
VladenCode <vladimir.cvejanov@gmail.com>
2026-02-02 14:36:16 +0100

    Adding split function to the asteroid objects after shooting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
+from logger import log_event
+import random
 
 
 class Asteroid(CircleShape):
@@ -12,5 +14,19 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         # must override
-        self.position += self.velocity * dt 
-
+        self.position += self.velocity * dt
+    
+    def split(self):
+        pygame.sprite.Sprite.kill(self)
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)
+            first_asteriod_direction = self.velocity.rotate(random_angle)
+            second_asteroid_direction = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            first_split_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            second_split_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            first_split_asteroid.velocity = first_asteriod_direction * 1.2
+            second_split_asteroid.velocity = second_asteroid_direction * 1.2
