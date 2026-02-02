e5fb4fc5c017a7abb2a7166444b61df9fc9d0549
VladenCode <vladimir.cvejanov@gmail.com>
2026-02-02 12:14:54 +0100

    Adding shooting posibilities

--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 from circleshape import CircleShape
+from shot import Shot
+
 
 class Player (CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -33,8 +36,20 @@ class Player (CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+        #shoot
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        new_shoot = Shot(self.position.x, self.position.y)
+        velocity = pygame.Vector2(0, 1).rotate(self.rotation)
+        new_shoot.velocity = velocity * PLAYER_SHOOT_SPEED
+
+
+
